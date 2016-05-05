d291d08e2f9256e43d8a67174b56c99174e67180
vitoriab <evitoria@gmail.com>
2016-05-05 14:40:40 -0600

    Panel controller

--- app.js
+++ app.js
@@ -6,6 +6,21 @@
         this.products = gems;        
     });
     
+    app.controller('PanelController', function(){
+        this.tab = 1;
+        
+        this.selectTab = function(setTab){
+            this.tab = setTab;
+        };
+        
+        this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+        };
+        
+        
+    });
+    
+    
     var gems = [
     
     {
